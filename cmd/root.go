1dc63462b85f63a0f5f6d1d0c79df7a4e4f22e67
Eric Smith <paytonrules@gmail.com>
2020-09-28 13:53:56 -0500

    Add a breakpoint

--- cmd/root.go
+++ cmd/root.go
@@ -20,6 +20,7 @@ import (
 	"errors"
 	"fmt"
 	"plugin"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -242,6 +243,7 @@ func exportTransformers() ([]event.TransformerInitializer, []storage.Transformer
 	}
 
 	LogWithCommand.Info("linking plugin ", pluginPath)
+	runtime.Breakpoint()
 	plug, openErr := plugin.Open(pluginPath)
 	LogWithCommand.Info("IT CAN'T - BE ITS IMPOSSIBLE", pluginPath)
 	if openErr != nil {
